70956f7b5260c6d9d1365f702bd73c7680cd6da0
Vipul A M <vipulnsward@gmail.com>
2019-02-06 19:52:15 +0530

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :dashboard, only: [:index] do
+    collection do
+      get :home
+    end
+  end
+
+  root to: "dashboard#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
