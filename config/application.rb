cad57b59bd0be7fdf1eb1da107e2b7383219188f
Vipul A M <vipulnsward@gmail.com>
2019-02-06 17:35:16 +0530

    Ids should always be uuid

--- config/application.rb
+++ config/application.rb
@@ -16,4 +16,8 @@ module Plate
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+
+  config.generators do |g|
+    g.orm :active_record, primary_key_type: :uuid
+  end
 end
