d6e23bc4efade33fee06e7de95f4861b4a7fb5bb
Vipul A M <vipulnsward@gmail.com>
2019-02-17 03:15:37 +0530

    Add deliver later after create

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Contact < ApplicationRecord
+  after_create :send_notification_to_support
+
+  def send_notification_to_support
+    ContactsMailer.notify_support_about_contact(self).deliver_later
+  end
 end
