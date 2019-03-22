11832106159cfc7040dd9894690063eb34fc6339
alkesh26 <alkesh26@gmail.com>
2019-03-22 22:35:10 +0530

    Admin user dashboard using administrate gem.

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Admin
+  class UsersController < Admin::ApplicationController
+    before_action :delete_password_if_blank, only: :update
+
+    private
+
+      # Method is overridden so that admin user can update details
+      # of any existing user from admin dashboard
+      # Check this link for reference
+      # https://github.com/thoughtbot/administrate/issues/495#issuecomment-249566344
+      def delete_password_if_blank
+        if params[:user][:password].blank?
+          params[:user].delete(:password)
+          params[:user].delete(:password_confirmation)
+        end
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: "registrations"}
+  namespace :admin do
+    resources :users
+    root to: "users#index"
+  end
+
+  devise_for :users, controllers: { registrations: "registrations" }
 
   # Authentication
   devise_scope :user do
