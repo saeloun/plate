e589bb352dc2aba0321bc46bb5f8d4c2aa206a0a
alkesh26 <alkesh26@gmail.com>
2019-03-24 02:09:50 +0530

    model test cases

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_user_is_admin_if_role_assigned_is_admin
+    user = users :admin
+    assert user.admin?
+  end
+
+  def test_user_is_not_admin_if_role_assigned_is_member
+    user = users :albert
+    assert_not user.admin?
+  end
+
+  def test_should_return_first_name_and_last_name_as_name
+    user = users :albert
+    assert_equal "Albert Smith", user.name
+  end
 end
