c93b99244cdbea54bcc5486522c084ea9ffb8488
alkesh26 <alkesh26@gmail.com>
2019-03-26 01:28:22 +0530

    Added model test cases (#38)
    * model test cases
    
    * Code review changes and added SimpleCov gem for code coverage

--- Gemfile
+++ Gemfile
@@ -51,6 +51,9 @@ group :test do
   gem "selenium-webdriver"
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem "chromedriver-helper"
+
+  # for test coverage report
+  gem "simplecov", require: false
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,72 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_user_is_admin_if_role_assigned_is_admin
+    user = users :admin
+    assert user.admin?
+  end
+
+  def test_user_is_not_admin_if_role_assigned_is_member
+    user = users :albert
+    assert_not user.admin?
+  end
+
+  def test_should_return_first_name_and_last_name_as_name
+    user = users :albert
+    assert_equal "Albert Smith", user.name
+  end
+
+  def test_should_return_error_if_email_is_blank
+    invalid_user = {
+                     first_name: 'Albert',
+                     last_name: 'Smith',
+                     password: 'welcome'
+                   }
+
+    user = User.new(invalid_user)
+
+    assert_not user.valid?
+    assert_equal ["can't be blank"], user.errors[:email]
+  end
+
+  def test_should_return_error_if_password_is_blank
+    invalid_user = {
+                     first_name: 'Steve',
+                     last_name: 'Smith',
+                     email: 'steve@example.com'
+                   }
+
+    user = User.new(invalid_user)
+
+    assert_not user.valid?
+    assert_equal ["can't be blank"], user.errors[:password]
+  end
+
+  def test_should_return_error_if_password_is_too_short
+    invalid_user = {
+                     first_name: 'Steve',
+                     last_name: 'Smith',
+                     email: 'steve@example.com',
+                     password: 'short'
+                   }
+
+    user = User.new(invalid_user)
+
+    assert_not user.valid?
+    assert_equal ["is too short (minimum is 6 characters)"], user.errors[:password]
+  end
+
+  def test_should_return_no_errors_if_user_details_are_valid
+    valid_user = {
+      first_name: 'Steve',
+      last_name: 'Smith',
+      email: 'steve@example.com',
+      password: 'welcome'
+    }
+
+    user = User.new(valid_user)
+
+    assert user.valid?
+    assert_equal({}, user.errors.messages)
+  end
 end
