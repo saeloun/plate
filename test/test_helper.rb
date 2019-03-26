e54355e1df684f729f62487bd4e8cfe960a76bc4
alkesh26 <alkesh26@gmail.com>
2019-03-26 22:12:11 +0530

    Added test cases for controllers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,7 @@ def enable_test_coverage
     add_filter "/test/"
 
     add_group "Models", "app/models"
+    add_group "Controllers", "app/controllers"
   end
 end
 
@@ -15,6 +16,10 @@ enable_test_coverage if ENV["COVERAGE"]
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+
+class ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+end
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
