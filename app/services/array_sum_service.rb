0202634b5ecd69c5a54f736db3e226ece5f0a084
alkesh26 <alkesh26@gmail.com>
2019-04-04 22:34:19 +0530

    Sample service class and test case (#44)
    * Sample service class and test case
    
    * Code review change. Raised error in case passed argument to ArraySumService is not an array.
    
    * Code review change. using is_a? instead of .class ==.

---
+++ app/services/array_sum_service.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class ArraySumService
+  attr_reader :array
+
+  def initialize(array)
+    @array = array
+    raise_error_if_argument_is_not_an_array
+  end
+
+  def process
+    array.inject(&:+)
+  end
+
+  private
+
+  def raise_error_if_argument_is_not_an_array
+    raise 'Passed argument is not an array' unless array.is_a? Array
+  end
+end
