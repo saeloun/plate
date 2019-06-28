94d15d8338baf76bac021156e55b29bfc84d379d
Romil Mehta <romil@officetroops.com>
2019-06-28 22:12:51 +0530

    Adding more test cases

--- test/jobs/contacts_mailer_job_test.rb
+++ test/jobs/contacts_mailer_job_test.rb
@@ -3,14 +3,22 @@
 require "test_helper"
 
 class ContactsMailerJobTest < ActiveJob::TestCase
-  def test_contacts_mailer_enqueue_job
-    contact = create(:contact, email: "test@example.com")
+  def setup
+    @contact = create(:contact, email: "test@example.com")
+  end
 
+  def test_contacts_mailer_enqueue_job
     assert_no_performed_jobs
 
     perform_enqueued_jobs do
-      ContactsMailerJob.perform_later(contact)
+      ContactsMailerJob.perform_later(@contact)
     end
     assert_performed_jobs 1
+  end
+
+  def test_enqueue_contacts_mailer_job_only
+    assert_no_performed_jobs except: ContactsMailerJob do
+      ContactsMailerJob.perform_later(@contact)
+    end
   end
 end
