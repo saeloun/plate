2e919005954d1063416a2a7cdaaba78e4a57b2e5
Romil Mehta <romilm@alphahealthventures.com>
2019-07-03 03:36:36 +0530

    Adding honeybadger configuration (#51)
    * Adding honeybadger configuration
    
    * Adding filter_keys to filter specific params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_honeybadger_context
+
+  private
+
+    def set_honeybadger_context
+      Honeybadger.context(
+        user_id: current_user&.id,
+        user_email: current_user&.email,
+        url: request.url
+      )
+    end
 end
