951cace7d5cd1e0610ad6358822da9e55e2d6fc3
Romil Mehta <romil@officetroops.com>
2019-07-03 08:38:19 +0530

    Removing send notification callback after creation of contact to avoid multiple sending of message

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 class Contact < ApplicationRecord
-  after_create :send_notification_to_support
-
   validates :email, :message, presence: true
-
-  def send_notification_to_support
-    ContactsMailer.notify_support_about_contact(self).deliver_later
-  end
 end
